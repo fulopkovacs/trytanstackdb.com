94e0b7471654caaf5e70f8a69b2c610959793528
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-14 21:46:51 +0100

    WIP: move a task to the end of a different column

--- src/collections/todoItems.ts
+++ src/collections/todoItems.ts
@@ -2,13 +2,33 @@ import { createCollection } from "@tanstack/db";
 import { queryCollectionOptions } from "@tanstack/query-db-collection";
 import * as TanstackQuery from "@/integrations/tanstack-query/root-provider";
 import { getProjectsQueryOptions } from "@/server/functions/getProjects";
-import { getTodoItems } from "@/server/functions/getTodoItems";
+import { getTodoItems, updateTodoItem } from "@/server/functions/getTodoItems";
 
 export const todoItemsCollection = createCollection(
   queryCollectionOptions({
     queryKey: getProjectsQueryOptions.queryKey,
     queryFn: getTodoItems,
     queryClient: TanstackQuery.getContext().queryClient,
+    onUpdate: async ({ transaction, collection }) => {
+      const { original, changes } = transaction.mutations[0];
+
+      // const allItems = collection.toArray;
+      // const oldIndex = original.positionIndexOnBoard;
+      // const newIndex = changes.positionIndexOnBoard;
+
+      // update
+      try {
+        await updateTodoItem({
+          data: {
+            ...original,
+            ...changes,
+          },
+        });
+      } catch (error) {
+        // TODO: handle error
+        console.error("Failed to update todo item:", error);
+      }
+    },
     getKey: (item) => item.id,
   }),
 );
