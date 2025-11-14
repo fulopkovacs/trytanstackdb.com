b497178d1000b4f7e7dc754408035d09123cb4b4
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-14 14:19:38 +0100

    Get tempDbId in the boards route

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,18 +1,30 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
 import { AppSidebar } from "@/components/AppSidebar";
 import { Header } from "@/components/Header";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { requireTempId } from "@/server/middlewares/getTempDbIdFromRequest";
 
 const mockUser = {
   id: "1",
   name: "John Doe",
 };
 
+const getTempDbId = createServerFn()
+  .middleware([requireTempId])
+  .handler(async ({ context: { tempId } }) => {
+    return tempId;
+  });
+
 export const Route = createFileRoute("/projects")({
   component: RouteComponent,
   beforeLoad: async () => {
+    // TODO: get tempdb
+    const tempDbId = await getTempDbId();
+
     return {
       user: mockUser,
+      tempDbId,
     };
   },
   loader: async ({ context }) => {
