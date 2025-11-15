dca72cff7eb7e035c0c54ad3d5b3ac5882a3432c
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-15 16:32:23 +0100

    Show project description.

--- src/routes/_tempDbRequired.projects.$projectId.tsx
+++ src/routes/_tempDbRequired.projects.$projectId.tsx
@@ -25,28 +25,12 @@ function RouteComponent() {
     [projectId],
   );
 
-  // const tasks = useMemo(() => {
-  //   const limit = 1500;
-  //   const tasks = [...mockTasks];
-  //
-  //   for (let i = 0; i < limit; i++) {
-  //     tasks.push({
-  //       id: `task-${i + mockTasks.length + 1}`,
-  //       title: `Task ${i + mockTasks.length + 1}`,
-  //       // status: mockTasks[i % mockTasks.length].status,
-  //       status: "todo",
-  //     });
-  //   }
-  //
-  //   // return tasks.slice(0, 7);
-  //   return tasks;
-  // }, []);
-
   return (
     <div className="px-4 py-2 flex flex-col gap-4 overflow-hidden flex-1 min-h-0">
       <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight text-balance">
         {project?.name || "Project"}
       </h1>
+      <p>{project?.description || ""}</p>
       <TodoBoards projectId={projectId} />
     </div>
   );
