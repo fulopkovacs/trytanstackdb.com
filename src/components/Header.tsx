381d60c16913962eb39ef9879b636b84898bf797
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-15 16:43:38 +0100

    Add a link to the tanstack db docs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,23 @@
+import { DatabaseZap } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
 
 export function Header() {
   return (
     <header className="flex items-center justify-between px-4 py-2 border-b bg-background sticky top-0">
+      <a
+        target="_blank"
+        href="https://tanstack.com/db/latest/docs/overview"
+        className="flex items-center gap-2 text-sm cursor-pointer"
+        rel="noopener"
+      >
+        <DatabaseZap className="h-4 text-orange-500" />
+        <div>
+          <span className="bg-linear-to-r bg-clip-text from-orange-500 to-orange-700 text-transparent">
+            TanTanck DB Demo{" "}
+          </span>
+          <span className="text-muted-foreground">(unofficial!)</span>{" "}
+        </div>
+      </a>
       <div className="ml-auto flex items-center gap-2">
         <ModeToggle />
       </div>
