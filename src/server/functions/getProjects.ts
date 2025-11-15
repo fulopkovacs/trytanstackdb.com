6d54237a4a8464d4baee0d6b10b4ec4ab4772f20
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-15 13:40:43 +0100

    Moving items between lists work

--- src/server/functions/getProjects.ts
+++ src/server/functions/getProjects.ts
@@ -1,9 +1,10 @@
 import { queryOptions } from "@tanstack/react-query";
 import { createServerFn } from "@tanstack/react-start";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "@/db";
 import { projectsTable } from "@/db/schema";
 import { requireTempId } from "../middlewares/getTempDbIdFromRequest";
+import z from "zod";
 
 export const getProjects = createServerFn()
   .middleware([requireTempId])
@@ -18,3 +19,30 @@ export const getProjectsQueryOptions = queryOptions({
   queryKey: ["projects"],
   queryFn: getProjects,
 });
+
+export const updateProject = createServerFn()
+  .middleware([requireTempId])
+  .inputValidator(
+    // Define the input schema as needed
+    // For simplicity, using a generic object here
+    z.object({
+      id: z.string(),
+      name: z.string().optional(),
+      description: z.string().optional(),
+      itemPositionsInTheProject: z
+        .record(z.string(), z.array(z.string()))
+        .optional(),
+    }),
+  )
+  .handler(async ({ data: { id, ...updatedData }, context: { tempId } }) => {
+    const columnsToUpdate = Object.keys(updatedData);
+
+    if (columnsToUpdate.length === 0) {
+      throw new Error("No columns to update");
+    }
+
+    await db
+      .update(projectsTable)
+      .set(updatedData)
+      .where(and(eq(projectsTable.id, id), eq(projectsTable.tempDbId, tempId)));
+  });
