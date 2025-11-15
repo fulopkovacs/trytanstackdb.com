03c9a091bb094764f8b38b43c137659c20cd37ac
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-15 22:46:26 +0100

    Make the todo items use the api too

--- src/collections/todoItems.ts
+++ src/collections/todoItems.ts
@@ -1,26 +1,51 @@
 import { createCollection } from "@tanstack/db";
 import { queryCollectionOptions } from "@tanstack/query-db-collection";
+import type { TodoItemRecord } from "@/db/schema";
 import * as TanstackQuery from "@/integrations/tanstack-query/root-provider";
-import { getProjectsQueryOptions } from "@/server/functions/getProjects";
-import { getTodoItems, updateTodoItem } from "@/server/functions/getTodoItems";
+import { getTodoItemsOptions } from "@/server/functions/getTodoItems";
+
+async function getTodoItems() {
+  const res = await fetch("/api/todo-items", { method: "GET" });
+
+  const todoItems: TodoItemRecord[] = await res.json();
+
+  return todoItems;
+}
+
+async function updateTodoItem({
+  data,
+}: {
+  data: Partial<TodoItemRecord> & { id: string };
+}) {
+  const res = await fetch("/api/todo-items", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to update todo item");
+  }
+
+  const updatedItem: TodoItemRecord = await res.json();
+
+  return updatedItem;
+}
 
 export const todoItemsCollection = createCollection(
   queryCollectionOptions({
-    queryKey: getProjectsQueryOptions.queryKey,
+    queryKey: getTodoItemsOptions.queryKey,
     queryFn: getTodoItems,
     queryClient: TanstackQuery.getContext().queryClient,
-    onUpdate: async ({ transaction, collection }) => {
+    onUpdate: async ({ transaction }) => {
       const { original, changes } = transaction.mutations[0];
 
-      // const allItems = collection.toArray;
-      // const oldIndex = original.positionIndexOnBoard;
-      // const newIndex = changes.positionIndexOnBoard;
-
-      // update
       try {
         await updateTodoItem({
           data: {
-            ...original,
+            id: original.id,
             ...changes,
           },
         });
