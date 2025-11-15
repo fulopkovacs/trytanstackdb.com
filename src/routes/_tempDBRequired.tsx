a3c5a60528947431a2a8edd742a2c2e2520bd07d
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-15 23:55:48 +0100

    well...

--- src/routes/_tempDBRequired.tsx
+++ src/routes/_tempDBRequired.tsx
@@ -1,10 +1,7 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { createIsomorphicFn, createServerFn } from "@tanstack/react-start";
-import { checkIfTempDbExists } from "@/server/functions/checkIfTempDbExists";
-import { getSubdomainAndApexFromHost } from "@/server/functions/getSubdomainAndApexFromHost";
 import { requireTempId } from "@/server/middlewares/getTempDbIdFromRequest";
 import { getTempDbIdFromTheSubdomain } from "@/utils/getTempDbIdFromSubdomain";
-import { getApexDomainRedirectHref } from "@/utils/server/getApexDomainRedirectHref";
 
 const mockUser = {
   id: "1",
@@ -39,25 +36,25 @@ export const Route = createFileRoute("/_tempDbRequired")({
       tempDbId,
     };
   },
-  loader: async ({ context }) => {
-    const { subdomain, apex, protocol } = await getSubdomainAndApexFromHost();
-
-    if (subdomain) {
-      const tempDbExists = await checkIfTempDbExists({
-        data: { tempDbId: subdomain },
-      });
-
-      if (!tempDbExists) {
-        // redirect to the apex
-        // throw new Response("Temporary database not found", { status: 404 });
-        throw redirect({
-          href: `${getApexDomainRedirectHref(apex, protocol)}?temp_db_missing=${subdomain}`,
-        });
-      }
-    }
-
-    return {
-      tempDbId: context.tempDbId,
-    };
-  },
+  // loader: async ({ context }) => {
+  //   const { subdomain, apex, protocol } = await getSubdomainAndApexFromHost();
+  //
+  //   if (subdomain) {
+  //     const tempDbExists = await checkIfTempDbExists({
+  //       data: { tempDbId: subdomain },
+  //     });
+  //
+  //     if (!tempDbExists) {
+  //       // redirect to the apex
+  //       // throw new Response("Temporary database not found", { status: 404 });
+  //       throw redirect({
+  //         href: `${getApexDomainRedirectHref(apex, protocol)}?temp_db_missing=${subdomain}`,
+  //       });
+  //     }
+  //   }
+  //
+  //   return {
+  //     tempDbId: context.tempDbId,
+  //   };
+  // },
 });
