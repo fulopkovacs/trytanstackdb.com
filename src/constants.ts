951988e9b38872d8fc139241639ba2495508b32f
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-15 14:16:50 +0100

    Make sure the /projects route redirects to login if the temp db is missing from the db

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,11 @@
 export const expiryTimestampMs = Date.now() + 60_000 * 60 * 24 * 365; // 1 yr
+export const getApexDomainRedirectHref = (
+  apex: string | undefined | null,
+  protocol: string,
+) => {
+  // TODO: NODE_ENV should be imported from process.env.NODE_ENV
+  if (!apex) {
+    throw new Error("Apex domain is required for redirect href");
+  }
+  return `${protocol}//${apex}/`;
+};
