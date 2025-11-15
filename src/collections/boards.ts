72a48a3b2d51616fdcb03022572490dfbb3c9e9d
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-15 22:23:42 +0100

    Make boards use the api

--- src/collections/boards.ts
+++ src/collections/boards.ts
@@ -1,7 +1,19 @@
 import { createCollection } from "@tanstack/db";
 import { queryCollectionOptions } from "@tanstack/query-db-collection";
+import type { BoardRecord } from "@/db/schema";
 import * as TanstackQuery from "@/integrations/tanstack-query/root-provider";
-import { getBoards, getBoardsQueryOptions } from "@/server/functions/getBoards";
+import { getBoardsQueryOptions } from "@/server/functions/getBoards";
+
+async function getBoards() {
+  const res = await fetch("/api/boards");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch boards");
+  }
+
+  const data = await res.json();
+  return data as BoardRecord[];
+}
 
 export const boardCollection = createCollection(
   queryCollectionOptions({
