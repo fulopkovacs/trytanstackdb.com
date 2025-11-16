39bd36dd17f5eb4f7280f9e784a9ce0dfcfed783
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-16 12:57:21 +0100

    Create todo items

--- src/components/projects.formComponents.tsx
+++ src/components/projects.formComponents.tsx
@@ -3,6 +3,7 @@ import { useId } from "react";
 import { useFieldContext } from "@/hooks/app.form-context";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { Textarea as ShadcnTextarea } from "./ui/textarea";
 
 function ErrorMessages({
   errors,
@@ -48,6 +49,35 @@ export function TextField({
         onChange={(e) => field.handleChange(e.target.value)}
       />
 
+      {field.state.meta.isTouched && <ErrorMessages errors={errors} />}
+    </div>
+  );
+}
+
+export function TextArea({
+  label,
+  rows = 3,
+}: {
+  label: string;
+  rows?: number;
+}) {
+  const field = useFieldContext<string>();
+  const errors = useStore(field.store, (state) => state.meta.errors);
+
+  const id = useId();
+
+  return (
+    <div>
+      <Label htmlFor={id} className="mb-2">
+        {label}
+      </Label>
+      <ShadcnTextarea
+        id={id}
+        value={field.state.value}
+        onBlur={field.handleBlur}
+        rows={rows}
+        onChange={(e) => field.handleChange(e.target.value)}
+      />
       {field.state.meta.isTouched && <ErrorMessages errors={errors} />}
     </div>
   );
--- src/routes/api.todo-items.ts
+++ src/routes/api.todo-items.ts
@@ -3,13 +3,28 @@ import { json } from "@tanstack/react-start";
 import { and, eq } from "drizzle-orm";
 import z from "zod";
 import { db } from "@/db";
-import { todoItemsTable } from "@/db/schema";
+import { projectsTable, TodoItemRecord, todoItemsTable } from "@/db/schema";
 import { getTempDbIdFromRequest } from "@/server/middlewares/getTempDbIdFromRequest";
 
-const todoItemUpdateData = z.object({
+const todoItemCreateData = z.object({
   id: z.string().min(1),
+  boardId: z.string(),
+  priority: z.number().min(0).max(3).int().optional().nullable(),
+  title: z.string(),
+  description: z.string().optional().nullable(),
+  createdAtTimestampMs: z.number().nullable(),
+  projectId: z.string(),
+  // tempDbId: z.string(),
+});
+
+export type TodoItemCreateDataType = z.infer<typeof todoItemCreateData>;
+
+const todoItemUpdateData = z.object({
+  id: z.string(),
   boardId: z.string().optional(),
-  priority: z.number().min(0).max(3).int().optional(),
+  priority: z.number().nullable().optional(),
+  title: z.string().optional(),
+  description: z.string().nullable().optional(),
 });
 
 export const Route = createFileRoute("/api/todo-items")({
@@ -23,6 +38,123 @@ export const Route = createFileRoute("/api/todo-items")({
           .where(eq(todoItemsTable.tempDbId, tempId));
 
         return json(results);
+      },
+      POST: async ({ context: { tempId }, request }) => {
+        // Create new todo item
+        let newTodoItemData: Omit<
+          z.infer<typeof todoItemCreateData>,
+          "projectId"
+        >;
+        let projectId: string;
+        // biome-ignore lint/suspicious/noExplicitAny: it can be any here
+        let bodyObj: any;
+
+        try {
+          bodyObj = await request.json();
+        } catch (e) {
+          console.error("Error parsing JSON body:", e);
+          return new Response(`Invalid JSON body`, { status: 400 });
+        }
+
+        try {
+          const { projectId: projectIdFromPayload, ...todoItemData } =
+            todoItemCreateData.parse(bodyObj);
+          projectId = projectIdFromPayload;
+          newTodoItemData = todoItemData;
+        } catch (e) {
+          console.error("Validation error:", e);
+          if (e instanceof z.ZodError) {
+            return new Response(`Invalid request data: ${z.prettifyError(e)}`, {
+              status: 400,
+            });
+          }
+          console.error("Bad format", e);
+          return new Response(`Validation error`, { status: 400 });
+        }
+
+        const { description } = newTodoItemData;
+
+        if (!description) {
+          return new Response(`Description is required`, { status: 400 });
+        }
+
+        await db.insert(todoItemsTable).values({
+          ...newTodoItemData,
+          description,
+          tempDbId: tempId,
+          createdAtTimestampMs:
+            newTodoItemData.createdAtTimestampMs || Date.now(),
+          priority: 0,
+        } satisfies TodoItemRecord);
+
+        try {
+          // Update the order index in the project
+          const [project] = await db
+            .select({
+              id: projectsTable.id,
+              itemPositionsInTheProject:
+                projectsTable.itemPositionsInTheProject,
+            })
+            .from(projectsTable)
+            .where(
+              and(
+                eq(projectsTable.id, projectId),
+                eq(projectsTable.tempDbId, tempId),
+              ),
+            );
+
+          if (!project) {
+            throw new Error(`Project not found: ${projectId}`);
+          }
+
+          project.itemPositionsInTheProject[newTodoItemData.boardId] =
+            project.itemPositionsInTheProject[newTodoItemData.boardId] || [];
+          project.itemPositionsInTheProject[newTodoItemData.boardId].unshift(
+            newTodoItemData.id,
+          );
+
+          await db
+            .update(projectsTable)
+            .set({
+              itemPositionsInTheProject: project.itemPositionsInTheProject,
+            })
+            .where(
+              and(
+                eq(projectsTable.id, project.id),
+                eq(projectsTable.tempDbId, tempId),
+              ),
+            );
+        } catch (e) {
+          // roll back
+          try {
+            await db
+              .delete(todoItemsTable)
+              .where(
+                and(
+                  eq(todoItemsTable.id, newTodoItemData.id),
+                  eq(todoItemsTable.tempDbId, tempId),
+                ),
+              );
+          } catch (deleteError) {
+            console.error(
+              "Error rolling back todo item after project update failure:",
+              deleteError,
+            );
+            return new Response("Todo item insert rollback failed", {
+              status: 500,
+            });
+          }
+
+          console.error("Database error:", e);
+          return new Response("Could not insert todo item", { status: 500 });
+        }
+
+        return json(
+          { created: "ok" },
+          {
+            status: 201,
+          },
+        );
       },
       PATCH: async ({ context: { tempId }, request }) => {
         let updatedData: z.infer<typeof todoItemUpdateData>;
