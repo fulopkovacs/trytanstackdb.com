99c23eceae000b43d55807a96e4e1dc1c8a6e6d0
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-16 16:08:06 +0100

    Create a base for the tutorial window

---
+++ src/components/tutorial/TutorialWindow.tsx
@@ -0,0 +1,9 @@
+export function TutorialWindow() {
+  return (
+    <div className="w-0 h-0">
+      <div className="max-w-1/2 w-[400px] absolute bottom-2 left-2 bg-white z-[100]">
+        hello
+      </div>
+    </div>
+  );
+}
