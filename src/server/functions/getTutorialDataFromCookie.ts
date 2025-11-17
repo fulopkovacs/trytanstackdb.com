9cb60d177bed380451e88be1739b502c3924290c
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-17 16:06:33 +0100

    Just a commit, nothing to see here

--- src/server/functions/getTutorialDataFromCookie.ts
+++ src/server/functions/getTutorialDataFromCookie.ts
@@ -10,15 +10,18 @@ export const getTutorialDataFromCookie = createServerFn().handler(
   async (): Promise<TutorialData> => {
     const tutorialData = getCookie(TUTORIAL_COOKIE_NAME);
 
-    try {
-      const data = tutorialDataSchema.parse(JSON.parse(tutorialData || "{}"));
-      return data;
-    } catch (e) {
-      console.error("Error parsing tutorial data from cookie:", e);
-      return {
-        tutorialStep: null,
-        scrollPositions: {},
-      };
+    if (tutorialData) {
+      try {
+        const data = tutorialDataSchema.parse(JSON.parse(tutorialData || "{}"));
+        return data;
+      } catch (e) {
+        console.error("Error parsing tutorial data from cookie:", e);
+      }
     }
+
+    return {
+      tutorialStep: null,
+      scrollPositions: {},
+    };
   },
 );
