f59a32be9cef7f6bad8774d4e34d444129bf6d30
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-22 12:39:01 +0100

    Fix some lint errors

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -97,7 +97,7 @@ function SidebarProvider({
   // Helper to toggle the sidebar.
   const toggleSidebar = useCallback(() => {
     return isMobile ? setOpenMobile((open) => !open) : setOpen((open) => !open);
-  }, [isMobile, setOpen, setOpenMobile]);
+  }, [isMobile, setOpen]);
 
   // Adds a keyboard shortcut to toggle the sidebar.
   useEffect(() => {
@@ -129,7 +129,7 @@ function SidebarProvider({
       setOpenMobile,
       toggleSidebar,
     }),
-    [state, open, setOpen, isMobile, openMobile, setOpenMobile, toggleSidebar],
+    [state, open, setOpen, isMobile, openMobile, toggleSidebar],
   );
 
   return (
