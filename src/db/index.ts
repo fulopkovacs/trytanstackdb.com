4a29eaa7fb3109030fa65e731f6fbc5513c54282
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-25 19:05:24 +0100

    Remove some comments

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,27 +1,5 @@
-// import { config } from "dotenv";
 import { PGlite } from "@electric-sql/pglite";
 import { drizzle } from "drizzle-orm/pglite";
-
-// export const getDb = async () => {
-//   if (typeof window === "undefined") {
-//     throw new Error("PGlite is only supported in the browser environment.");
-//   }
-//   const { PGlite } = await import("@electric-sql/pglite");
-//   const { drizzle } = await import("drizzle-orm/pglite");
-//
-//   const dbName = import.meta.env.DEV
-//     ? "idb://DEV-trytanstackdb"
-//     : "idb://trytanstackdb";
-//
-//   const client = new PGlite(dbName);
-//
-//   const db = drizzle({
-//     client,
-//   });
-//   return { db, client };
-// };
-//
-// export type DB = Awaited<ReturnType<typeof getDb>>["db"];
 
 const dbName = import.meta.env.DEV
   ? "idb://DEV-trytanstackdb"
