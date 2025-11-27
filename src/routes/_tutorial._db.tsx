ed81f0e36d0cb4313a0ad1cfd403f413423ed99d
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-28 00:32:32 +0100

    Cloudflare could not handle the previous commit

--- src/routes/_tutorial._db.tsx
+++ src/routes/_tutorial._db.tsx
@@ -3,12 +3,7 @@ import {
   setupPGlite,
   setupServiceWorkerHttpsProxy,
 } from "@/client/pgliteHelpers";
-import { boardCollection } from "@/collections/boards";
-import { projectsCollection } from "@/collections/projects";
-import { todoItemsCollection } from "@/collections/todoItems";
 import { FakeProgressIndicator } from "@/components/FakeProgressIndicator";
-
-const COLLECTIONS_ARE_READY_GLOBAL_VAR_NAME = "__COLLECTIONS_ARE_READY__";
 
 export const Route = createFileRoute("/_tutorial/_db")({
   // the database is local to the browser, so no SSR
@@ -18,26 +13,7 @@ export const Route = createFileRoute("/_tutorial/_db")({
     await setupServiceWorkerHttpsProxy();
     await setupPGlite();
   },
-  loader: async () => {
-    /*
-      NOTE: this looks terrible, but I needed a quick way to ensure
-      that the fake progress bar is shown until all the collections
-      are loaded.
-
-      This hacky solution seemed to be the easiest: the pending component
-      will be shown until all promises are resolved in the loader,
-      and the global variable is used to to make sure that the statements
-      in the if block are not executed on client-side route changes.
-    */
-    // @ts-expect-error
-    if (!window[COLLECTIONS_ARE_READY_GLOBAL_VAR_NAME]) {
-      await projectsCollection.stateWhenReady();
-      await boardCollection.stateWhenReady();
-      await todoItemsCollection.stateWhenReady();
-      // @ts-expect-error
-      window[COLLECTIONS_ARE_READY_GLOBAL_VAR_NAME] = true;
-    }
-  },
+  loader: async () => {},
   component: RouteComponent,
   pendingComponent: FakeProgressIndicator,
   // Show the pending component for at least 500ms
