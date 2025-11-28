c235fe84cf32b7ce794771b389dafb7c9b1c4446
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-11-28 19:07:46 +0100

    Remove some code that was commented out

--- src/routes/_tutorial._db.projects.$projectId.tsx
+++ src/routes/_tutorial._db.projects.$projectId.tsx
@@ -4,13 +4,6 @@ import { EditableProjectDetails } from "@/components/EditableProjectDetails";
 import { TodoBoards } from "@/components/TodoBoards";
 
 export const Route = createFileRoute("/_tutorial/_db/projects/$projectId")({
-  // loader: async ({ params: { projectId } }) => {
-  //   const projectCollectionArray = await projectsCollection.toArrayWhenReady();
-  //   if (!projectCollectionArray.find((p) => p.id === projectId)) {
-  //     throw notFound();
-  //   }
-  // },
-  // notFoundComponent: NotFoundComponent,
   component: RouteComponent,
 });
 
@@ -25,16 +18,6 @@ function SharedLayout({ children }: { children: ReactNode }) {
 function RouteComponent() {
   const { projectId } = Route.useParams();
 
-  // const {
-  //   data: [project],
-  // } = useLiveQuery(
-  //   (q) =>
-  //     q
-  //       .from({ project: projectsCollection })
-  //       .where(({ project }) => eq(project.id, projectId)),
-  //   [projectId],
-  // );
-
   return (
     <SharedLayout>
       <EditableProjectDetails projectId={projectId} />
@@ -42,33 +25,3 @@ function RouteComponent() {
     </SharedLayout>
   );
 }
-
-// function NotFoundComponent() {
-//   const {
-//     data: [firstProject],
-//   } = useLiveQuery((q) =>
-//     q
-//       .from({ project: projectsCollection })
-//       .orderBy(({ project }) => project.createdAt)
-//       .limit(1),
-//   );
-//
-//   return (
-//     <SharedLayout>
-//       <div>
-//         <p>Project not found.</p>
-//         {firstProject && (
-//           <Link
-//             className="underline hover:opacity-80 transition-opacity text-orange-500"
-//             to={"/projects/$projectId"}
-//             params={{
-//               projectId: firstProject.id,
-//             }}
-//           >
-//             You can visit the "{firstProject.name}" project instead.
-//           </Link>
-//         )}
-//       </div>
-//     </SharedLayout>
-//   );
-// }
