e6a0de4852808aa1a8b50bbe59d43c5e4b331fe4
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-12-07 14:45:57 +0100

    Fix the animation of the FloatingWindowTrigger

---
+++ src/components/ToggleFloatingWindowButton.tsx
@@ -0,0 +1,92 @@
+import { DatabaseZapIcon } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import { useMemo, useRef } from "react";
+
+export function FloatingWindowButton({
+  isClosed,
+  activeStep,
+  toggleWindow,
+}: {
+  toggleWindow: () => void;
+  isClosed: boolean;
+  activeStep: string | null;
+}) {
+  const referenceContainerRef = useRef<HTMLDivElement>(null);
+
+  const containerWidth = useMemo(() => {
+    if (isClosed && referenceContainerRef.current) {
+      return referenceContainerRef.current.offsetWidth;
+    } else {
+      // default width
+      return 56;
+    }
+  }, [isClosed]);
+
+  const rotationDegrees = useMemo(() => {
+    return isClosed ? 0 : 360;
+  }, [isClosed]);
+
+  return (
+    <>
+      <motion.button
+        className="relative min-w-14 h-14 text-black inline-flex items-center justify-center gap-2 cursor-pointer bg-orange-500 py-4 px-0 rounded-full hover:bg-orange-600"
+        type="button"
+        onClick={() => toggleWindow()}
+        animate={{
+          // width: isClosed ? "auto" : "56px",
+          width: `${containerWidth.toString()}px`,
+        }}
+        initial={false}
+        transition={{
+          duration: 0.2,
+          ease: "easeInOut",
+        }}
+      >
+        <motion.div
+          animate={{
+            rotate: rotationDegrees,
+          }}
+          className="absolute top-0 left-0 h-full flex items-center justify-center w-14 shrink-0"
+        >
+          <DatabaseZapIcon className="block" />
+        </motion.div>
+        <div className="w-6 h-14" />
+        <AnimatePresence>
+          {isClosed && (
+            <div className="mx-4 h-0 pointer-events-none opacity-0">
+              {activeStep}
+            </div>
+          )}
+        </AnimatePresence>
+        <motion.div
+          initial={false}
+          className="absolute right-0 top-0 m-4"
+          animate={{
+            opacity: isClosed ? 1 : 0,
+          }}
+          transition={{
+            duration: isClosed ? 0.2 : 0,
+            ease: "easeInOut",
+            delay: isClosed ? 0.2 : 0,
+          }}
+        >
+          {activeStep}
+        </motion.div>
+      </motion.button>
+      {/*
+        This is a reference container that we use
+        to calculate the width of the button
+      */}
+      <div
+        ref={referenceContainerRef}
+        className="relative min-w-14 h-0 text-black inline-flex items-center justify-center gap-2 pointer-none"
+        aria-hidden="true"
+      >
+        <div className="w-6" aria-hidden="true" />
+        <div className="mx-4 h-0 overflow-hidden" aria-hidden="true">
+          {activeStep}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/tutorial/TutorialWindow.tsx
+++ src/components/tutorial/TutorialWindow.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/utils/getTutorialDataHandlers";
 import { Button } from "../ui/button";
 import { ScrollArea } from "../ui/scroll-area";
+import { FloatingWindowButton } from "../ToggleFloatingWindowButton";
 
 function FloatingWindowHeader({ toggleWindow }: { toggleWindow: () => void }) {
   return (
@@ -482,46 +483,11 @@ export function TutorialWindow({
               />
             </motion.div>
           )}
-          <motion.button
-            className="relative min-w-14 h-14 text-black inline-flex items-center justify-center gap-2 cursor-pointer bg-orange-500 py-4 px-0 rounded-full hover:bg-orange-600"
-            type="button"
-            onClick={() => toggleWindow()}
-            animate={{
-              width: isClosed ? "auto" : "56px",
-            }}
-            initial={false}
-            transition={{
-              duration: 0.2,
-              delay: isClosed ? 0 : 0.2,
-              ease: "easeInOut",
-            }}
-          >
-            <div className="absolute top-0 left-0 h-full flex items-center justify-center w-14 shrink-0">
-              <DatabaseZapIcon className="block" />
-            </div>
-            <div className="w-6 h-14" />
-            <AnimatePresence>
-              {isClosed && (
-                <div className="mx-4 h-0 pointer-events-none opacity-0">
-                  {activeStep}
-                </div>
-              )}
-            </AnimatePresence>
-            <motion.div
-              initial={false}
-              className="absolute right-0 top-0 m-4"
-              animate={{
-                opacity: isClosed ? 1 : 0,
-              }}
-              transition={{
-                duration: isClosed ? 0.2 : 0,
-                ease: "easeInOut",
-                delay: isClosed ? 0.2 : 0,
-              }}
-            >
-              {activeStep}
-            </motion.div>
-          </motion.button>
+          <FloatingWindowButton
+            isClosed={isClosed}
+            activeStep={activeStep}
+            toggleWindow={toggleWindow}
+          />
         </div>
       </div>
     </AnimatePresence>
