b363f23459a10d5afc3b53fcf270d8b2fe06f978
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-12-21 16:52:20 +0100

    Load the theme from tweakcn

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -35,6 +35,19 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     ],
     links: [
       {
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
+      },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap",
+      },
+      {
         rel: "stylesheet",
         href: appCss,
       },
