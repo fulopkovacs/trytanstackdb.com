f3e20943b1de0ee2a0c6b5d4d3e92e99602f3983
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-12-23 00:15:43 +0100

    Update the author attribution

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { ClientOnly } from "@tanstack/react-router";
-import { BugIcon, DatabaseZap, GithubIcon } from "lucide-react";
+import { BugIcon, GithubIcon } from "lucide-react";
 import { ConfigureDB } from "./ConfigureDB";
 import { ModeToggle } from "./mode-toggle";
 import { NetworkLatencyConfigurator } from "./NetworkLatencyConfigurator";
@@ -10,33 +10,32 @@ import { SidebarTrigger } from "./ui/sidebar";
 export function Header() {
   return (
     <header className="flex gap-2 items-center justify-between px-4 py-2 border-b bg-background sticky top-0">
-      <SidebarTrigger className="md:hidden" />
-      <a
-        target="_blank"
-        href="https://tanstack.com/db/latest/docs/overview"
-        className="flex items-center text-sm cursor-pointer"
-        rel="noopener"
-      >
-        <DatabaseZap className="h-4 text-orange-500" />
-        <span className="cursor-pointer bg-linear-to-r bg-clip-text from-orange-500 to-orange-700 text-transparent">
-          TanStack DB Tutorial{" "}
-        </span>
-      </a>
-      <span className="text-sm">ALPHA</span>
-      <span className="text-muted-foreground text-sm">by</span>
-      <a
-        href="https://fulop.dev"
-        target="_blank"
-        rel="noreferrer"
-        className="cursor-pointer text-muted-foreground hover:text-foreground transition-colors flex items-center gap-2 text-sm"
-      >
-        <img
-          alt="fuko"
-          src="https://avatars.githubusercontent.com/u/43729152?s=96&v=4"
-          className="inline h-6 w-6 rounded-full"
-        />
-        fuko
-      </a>
+      <div className="flex items-center gap-1 text-sm font-medium">
+        <a
+          href="https://fulop.dev"
+          target="_blank"
+          rel="noreferrer"
+          className="group/fuko-link cursor-pointer transition-colors flex items-center gap-2"
+        >
+          <img
+            alt="fuko"
+            src="https://avatars.githubusercontent.com/u/43729152?s=96&v=4"
+            className="inline h-6 w-6 rounded-full border"
+          />
+          <span className="underline decoration-wavy group-hover/fuko-link:opacity-80 transition-opacity">
+            fuko's
+          </span>
+        </a>
+        guide to <SidebarTrigger className="md:hidden" />
+        <a
+          target="_blank"
+          href="https://tanstack.com/db/latest/docs/overview"
+          className="text-primary flex items-center cursor-pointer underline decoration-wavy hover:opacity-80"
+          rel="noopener"
+        >
+          @tanstack/db
+        </a>
+      </div>
       <ClientOnly fallback={null}>
         <div className="ml-auto">
           <ConfigureDB />
