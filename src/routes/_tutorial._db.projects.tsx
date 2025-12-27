4380fcfe1313a3afba19b9db4de54ae09669f2ce
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-12-27 23:06:41 +0100

    Animate the api request panel

--- src/routes/_tutorial._db.projects.tsx
+++ src/routes/_tutorial._db.projects.tsx
@@ -1,16 +1,8 @@
-import { eq, useLiveQuery } from "@tanstack/react-db";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { userPreferencesCollection } from "@/collections/UserPreferences";
 import { ApiRequestsPanel } from "@/components/ApiRequestsPanel";
 import { AppSidebar } from "@/components/AppSidebar";
 import { Header } from "@/components/Header";
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { USER_PLACEHOLDER } from "@/utils/USER_PLACEHOLDER_CONSTANT";
 
 export const Route = createFileRoute("/_tutorial/_db/projects")({
   component: RouteComponent,
@@ -25,44 +17,16 @@ export const Route = createFileRoute("/_tutorial/_db/projects")({
 });
 
 function MainLayout() {
-  const { data: userPreferences } = useLiveQuery((q) =>
-    q
-      .from({
-        userPreferences: userPreferencesCollection,
-      })
-      .where(({ userPreferences }) =>
-        eq(userPreferences.id, USER_PLACEHOLDER.id),
-      )
-      .findOne(),
-  );
-
   return (
     <main className="flex flex-1 flex-col overflow-hidden max-h-screen h-screen">
       <Header />
-      <ResizablePanelGroup direction="horizontal" className="flex-1 min-h-0">
-        <ResizablePanel
-          defaultSize={userPreferences?.networkPanel === "open" ? 75 : 100}
-          minSize={50}
-        >
-          <div className="h-full flex relative">
-            <AppSidebar />
-            <Outlet />
-          </div>
-        </ResizablePanel>
-        {userPreferences?.networkPanel === "open" && (
-          <>
-            <ResizableHandle withHandle />
-            <ResizablePanel
-              defaultSize={25}
-              minSize={15}
-              maxSize={50}
-              className="max-w-sm"
-            >
-              <ApiRequestsPanel />
-            </ResizablePanel>
-          </>
-        )}
-      </ResizablePanelGroup>
+      <div className="flex flex-1 min-h-0 overflow-hidden">
+        <div className="flex-1 flex h-full relative min-w-0">
+          <AppSidebar />
+          <Outlet />
+        </div>
+        <ApiRequestsPanel />
+      </div>
     </main>
   );
 }
@@ -71,7 +35,7 @@ function RouteComponent() {
   // const { data: projects } = useSuspenseQuery(getProjectsQueryOptions);
 
   return (
-    <SidebarProvider className="w-auto" defaultOpen>
+    <SidebarProvider className="w-auto overflow-hidden" defaultOpen>
       <MainLayout />
     </SidebarProvider>
   );
