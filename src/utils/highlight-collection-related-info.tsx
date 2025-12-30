e6944ccba857a21b4828090404f9b86b68ec3224
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-12-30 12:09:01 +0100

    Refactor the highlight elements feature

--- src/utils/highlight-collection-related-info.tsx
+++ src/utils/highlight-collection-related-info.tsx
@@ -1,5 +1,5 @@
-import { useSearch } from "@tanstack/react-router";
-import type { ReactNode } from "react";
+import { useNavigate, useSearch } from "@tanstack/react-router";
+import { type ReactNode, useEffect } from "react";
 import type { HighlightParam } from "@/components/tutorial";
 import { cn } from "@/lib/utils";
 
@@ -13,13 +13,34 @@ export function HighlightWrapper({
   // asChild?: boolean;
 }) {
   const searchParams = useSearch({ strict: false });
+  const navigate = useNavigate();
+
+  const isHighlighted =
+    searchParams.highlight && highlightId.startsWith(searchParams.highlight);
+
+  useEffect(() => {
+    if (!isHighlighted) return;
+
+    const timeout = setTimeout(() => {
+      navigate({
+        to: ".",
+        search: ({ highlight: _, ...old }) => ({ ...old }),
+        replace: true,
+      });
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [isHighlighted, navigate]);
 
   return (
     <div
+      aria-hidden="true"
       className={cn(
-        searchParams.highlight &&
-          highlightId.startsWith(searchParams.highlight) &&
-          "outline-red-400 outline",
+        "outline outline-offset-4 transition-all ease-in-out rounded-sm",
+        isHighlighted
+          ? "outline-destructive bg-destructive/10 duration-300"
+          : "outline-destructive/0 bg-destructive/0 duration-1000",
+        // "outline-destructive bg-destructive/10 outline rounded-sm outline-offset-4",
       )}
     >
       {children}
